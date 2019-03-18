59949466b6deb40d0e7d9d82571ba3e56d4a13b8
Sarah Wright <s.wright.618@gmail.com>
2019-03-18 11:49:31 -0700

    Get rid of start/end time check constraints

--- mojn-sv-database/data/Tables/TreeCountActivity.sql
+++ mojn-sv-database/data/Tables/TreeCountActivity.sql
@@ -11,9 +11,7 @@
     [DateCreated]             DATETIME2 (0)  CONSTRAINT [DF_TreeCountActivity_DateCreated] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_TreeCountActivity] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [CK_TreeCountActivity_DataProcessingLevelNote_DisallowZeroLength] CHECK (len([DataProcessingLevelNote])>(0)),
-    CONSTRAINT [CK_TreeCountActivity_EndTime_Range] CHECK ([EndTime]>[StartTime] AND [EndTime]>=CONVERT([datetime2](0),'5am',(101)) AND [EndTime]<=CONVERT([datetime2](0),'8pm',(101))),
     CONSTRAINT [CK_TreeCountActivity_Notes_DisallowZeroLength] CHECK (len([Notes])>(0)),
-    CONSTRAINT [CK_TreeCountActivity_StartTime_Range] CHECK ([StartTime]>=CONVERT([datetime2](0),'5am',(101)) AND [StartTime]<=CONVERT([datetime2](0),'8pm',(101))),
     CONSTRAINT [FK_TreeCountActivity_DataProcessingLevel] FOREIGN KEY ([DataProcessingLevelID]) REFERENCES [lookup].[DataProcessingLevel] ([ID]),
     CONSTRAINT [FK_TreeCountActivity_Transect] FOREIGN KEY ([TransectID]) REFERENCES [data].[Transect] ([ID]),
     CONSTRAINT [FK_TreeCountActivity_Visit] FOREIGN KEY ([VisitID]) REFERENCES [data].[Visit] ([ID])
