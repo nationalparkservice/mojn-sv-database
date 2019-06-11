17cd410d73ab3b60e9f5d744cdf560c727a784db
Sarah Wright <s.wright.618@gmail.com>
2019-06-11 12:27:47 -0700

    Include field season in analysis views

--- mojn-sv-database/analysis/LPITransect.sql
+++ mojn-sv-database/analysis/LPITransect.sql
@@ -5,6 +5,7 @@
 			s.Name AS SpringName,
 			sc.Label AS SpringCategory,
 			vt.Label AS VisitType,
+			v.FieldSeason AS FieldSeason,
 			CONVERT(VARCHAR(10), v.StartDate, 101) AS StartDate,
 			CONVERT(VARCHAR(8), lpi.StartTime, 108) AS StartTime,
 			CONVERT(VARCHAR(8), lpi.EndTime, 108) AS EndTime,
